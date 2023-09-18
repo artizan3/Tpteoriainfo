448a2b64a1df7522224b8d04e8522866fffc0359
artizan3 <129905900+artizan3@users.noreply.github.com>
2023-09-18 14:11:39 -0300

    tp1

--- tp1.js
+++ tp1.js
@@ -1,14 +1,22 @@
-const fs = require('fs');
-const archivo = fs.openSync('a.txt', 'r');
-
+leerProximoBit();
 function leerProximoBit() {
-    if (bitsLeidos % 8 === 0) {
-      // Si hemos leído 8 bits (1 byte), leemos el próximo byte del archivo
-      const bytesRead = fs.readSync(archivo, buffer, 0, 1, null);
-      if (bytesRead === 0) {
-        // Hemos llegado al final del archivo
-        fs.closeSync(archivo);
-        return null;
-      }
+    const fs = require('fs');
+    const archivo = fs.openSync('prueba.bin', 'r');
+    var matbin=new Array;
+    matbin=[[0,0],[0,0]];
+
+    const buffer = Buffer.alloc(1);
+
+    let bytesRead;
+    let a=-1;
+    while ((bytesRead = fs.readSync(archivo, buffer, 0, 1, null)) !== 0) {
+        for (let i=7;i>=0;i--){
+            var b=(buffer[0]>>i)&1;
+            if (a!=-1)
+                matbin[b][a]++;
+            a=b;
+        }
     }
+    console.log(matbin);
+    fs.closeSync(archivo);
 }
