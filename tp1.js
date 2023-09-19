6f269610dead6d529160e1b172a17971fa7e406a
artizan3 <129905900+artizan3@users.noreply.github.com>
2023-09-18 23:47:10 -0300

    tp1

--- tp1.js
+++ tp1.js
@@ -8,7 +8,7 @@ console.log("matriz de probabilidades:",matbin);
 if (isnula(matbin))
     console.log("entropia nula:",entropianula(matbin));
 else
-    console.log("entropia no nula:",entropiNOnula(matbin));
+    console.log("entropia no nula:",entropiaNOnula(matbin));
 function lecturaArch(matbin,vecbin) {
     const fs = require('fs');
     if (process.argv[2]!=undefined && fs.existsSync(process.argv[2])){
@@ -59,10 +59,19 @@ function entropianula(matbin){
         sum*=process.argv[3];
     return sum;
 }
-function entropiNOnula(matbin){
+function entropiaNOnula(matbin){
     const math = require('mathjs');
     var a=[[matbin[0][0],matbin[0][1]],[1,1]];
     var b=[0,1];
     var sn=math.usolve(a,b);
-    
+    let aux=0,sum=0;
+    for (let i=0;i<2;i++){
+        for (let j=0;j<2;j++){
+            if (matbin[j][i]!=0)
+                aux+=matbin[j][i]*math.log2(1/matbin[j][i]);
+        }
+        aux*=sn[i];
+        sum+=aux;
+    }
+    return sum;
 }
